36cf8709d035fc779aab201e363c210390c01fa2
Aryan Kumar <aryankumar703394@gmail.com>
2026-07-30 17:45:12 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://realtimechatapp-av4o.onrender.com",
     credentials:true
 }))
 app.use(express.json())
